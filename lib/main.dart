4f3e26de8c6ede7cf2985ec81c275f43e3c8af4e
rahmat058 <md.kazi.rahmat@gmail.com>
2020-01-03 03:19:46 +0600

    List widget w is done

--- lib/main.dart
+++ lib/main.dart
@@ -7,85 +7,23 @@ void main() {
 
 
 class MyApp extends StatelessWidget {
+
+  List<String> nameOfStudents = ['Anul', 'Kader', 'Jabbar', 'Rahamt', 'Babu', 'Sagor'];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              height: 250,
-              color: Colors.blueAccent,
-              margin: EdgeInsets.all(20),
-              padding: EdgeInsets.all(20),
-              child: Image.asset("assets/600_466219125.jpeg", fit: BoxFit.cover,)
-            ),
-
-            Container(
-              width: double.infinity,
-              color: Colors.grey[300],
-              padding: EdgeInsets.only(top: 8, bottom: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.call, color: Colors.blueAccent, size: 40,),
-                      Container(
-                        height: 5,
-                      ),
-                      Text("Call", style: TextStyle(color: Colors.blueAccent, fontSize: 16, fontWeight: FontWeight.bold),)
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.call, color: Colors.blueAccent, size: 40,),
-                      Container(
-                        height: 5,
-                      ),
-                      Text("Call", style: TextStyle(color: Colors.blueAccent, fontSize: 16, fontWeight: FontWeight.bold),)
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.call, color: Colors.blueAccent, size: 40,),
-                      Container(
-                        height: 5,
-                      ),
-                      Text("Call", style: TextStyle(color: Colors.blueAccent, fontSize: 16, fontWeight: FontWeight.bold),)
-                    ],
-                  )
-                ],
-              )
-            ),
-
-            Container(
-              width: double.infinity,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      width: 150,
-                      height: 100,
-                      color: Colors.deepOrangeAccent,
-                    ),
-                    flex: 2,
-                  )
-                  ,
-                  Expanded(
-                    child: Container(
-                      width: 150,
-                      height: 100,
-                      color: Colors.deepPurpleAccent,
-                    ),
-                  )
-
-                ],
-              ),
-            )
-          ],
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(nameOfStudents[index]),
+              subtitle: Text('This is subtitle'),
+              leading: Icon(Icons.add_shopping_cart),
+            );
+          },
+          itemCount: nameOfStudents.length,
         ),
 
         appBar: AppBar(
